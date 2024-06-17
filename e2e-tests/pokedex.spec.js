b6808f8a554a28f5c571822bb1b93c8b60651867
teemukoukkari <teemu.koukkari@gmail.com>
2024-06-17 10:31:30 +0300

    Testing a page of a particular Pokemon

--- e2e-tests/pokedex.spec.js
+++ e2e-tests/pokedex.spec.js
@@ -5,5 +5,11 @@ describe('Pokedex', () => {
     await page.goto('')
     await expect(page.getByText('ivysaur')).toBeVisible()
     await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
+  }),
+  test('a page of a particular Pokemon can be viewed', async ({ page }) => {
+    await page.goto('')
+    await expect(page.getByText('ivysaur')).toBeVisible()
+    await page.getByText('ivysaur').click()
+    await expect(page.getByText('chlorophyll')).toBeVisible()
   })
 })
